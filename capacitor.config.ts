6d77933ab0d4d839804e22963867e01e60c7d0e7
nkululekomoss <mosss6150@gmail.com>
2024-10-26 00:49:25 +0200

    Kick Starter

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.reciclica',
+  appName: 'reciclica',
+  webDir: 'www'
+};
+
+export default config;
